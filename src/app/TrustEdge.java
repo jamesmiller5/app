ade75d1002d0e3ddd00e2f14841c033bb35b2b4c
bh3 <blackheartiii@gmail.com>
2013-09-13 19:48:29 -0400

    Added cost for trust edge and subject.

--- src/app/TrustEdge.java
+++ src/app/TrustEdge.java
@@ -4,6 +4,14 @@ import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.GraphDatabaseService;
 
-public class TrustEdge {
+public class TrustEdge extends Entity {
+	private static final String SUBJECT = "subject";
 
+	public TrustEdge( final Node internalNode ) {
+		super.initialize(internalNode);
+	}
+
+	public TrustEdge( final User from, final User to, final Subject subject ) {
+
+	}
 }
