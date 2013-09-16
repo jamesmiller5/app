8a7d0427776edd752e586e6ee072841e7486291f
James Miller V <mille168@purdue.edu>
2013-09-16 19:49:40 -0400

    Switched deps from an argument parser to shell parser, fixed bugs in build, test and make-jar scripts

--- src/app/Cli.java
+++ src/app/Cli.java
@@ -1,40 +1,22 @@
 package app;
 
-import com.martiansoftware.jsap.*;
+import asg.cliche.Command;
+import asg.cliche.ShellFactory;
+import java.io.IOException;
 
 public class Cli {
-	public static void main(String[] args) throws Exception {
-		System.out.println("Hello from cli");
-/*
-		SimpleJSAP jsap = new SimpleJSAP(
-			"MyProgram",
-			"Repeats \"Hello, world!\" multiple times",
-			new Parameter[] {
-				new FlaggedOption( "count", JSAP.INTEGER_PARSER, "1", JSAP.REQUIRED, 'n', JSAP.NO_LONGFLAG,
-					"The number of times to say hello." ),
-				new QualifiedSwitch( "verbose", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED, 'v', "verbose",
-					"Requests verbose output." ).setList( true ).setListSeparator( ',' ),
-				new UnflaggedOption( "name", JSAP.STRING_PARSER, "World", JSAP.REQUIRED, JSAP.GREEDY,
-					"One or more names of people you would like to greet." )
-			}
-		);
+	@Command // One,
+	public String hello() {
+			return "Hello, World!";
+	}
 
-		JSAPResult config = jsap.parse(args);
-		if( jsap.messagePrinted() ) System.exit( 1 );
+	@Command // two,
+	public int add(int a, int b) {
+			return a + b;
+	}
 
-		String[] names = config.getStringArray("name");
-		String[] languages = config.getStringArray("verbose");
-		for( int i = 0; i < languages.length; i++ ) {
-			System.out.println("language=" + languages[i]);
-		}
-		for( int i = 0; i < config.getInt("count"); i++ ) {
-			for( int j = 0; j < names.length; j++ ) {
-				System.out.println((config.getBoolean("verbose") ? "Hello" : "Hi")
-						+ ", "
-						+ names[j]
-						+ "!");
-			}
-		}
-*/
+	public static void main(String[] args) throws IOException {
+		ShellFactory.createConsoleShell("hello", "", new Cli())
+			.commandLoop(); // and three.
 	}
 }
