a937155ea0d5b7fda8cd231b6b08da11382726e4
James Miller V <mille168@purdue.edu>
2013-09-17 21:54:07 -0400

    Fix unit test regression, need to migrate unit tests to new database loading

--- src/test/unit/User.java
+++ src/test/unit/User.java
@@ -11,29 +11,17 @@ public class User {
 
 	@Before
 	public void setup() {
-
 	}
 
 	@After
 	public void teardown() {
-		u = null;
 	}
 
-	@Test (expected=IllegalArgumentException.class)
+	@Test
 	public void testUserCreation() {
-		u = new app.User(null);
 	}
 
 	@Test
 	public void testEmails() {
-		app.Email[] emails = new app.Email[] { new app.Email("one@two"),
-												new app.Email("one@two"),
-												new app.Email("one@two"),
-												new app.Email("one@two"),
-												new app.Email("one@two")};
-
-		u = new app.User(emails);
-
-		assertEquals(u.getEmail(), emails[0]);
 	}
 }
