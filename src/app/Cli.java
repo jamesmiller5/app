10e5e5f3027830cadb31258fcfb9cea76f5b3110
Andrew B. Mack <abmack@sac12.cs.purdue.edu>
2013-09-19 23:10:50 -0400

    added more integration tests for registration

--- src/app/Cli.java
+++ src/app/Cli.java
@@ -88,12 +88,15 @@ public class Cli {
 	public Result signup( String email ) {
 		Email e = null;
 		try {
+			//check if email exists
 			e = new Email(Entity.findExistingNode(LabelDef.EMAIL, Email.EMAIL_KEY, email));
 		}catch(IllegalStateException ise) {
+			//email doesn't exist, create and return claimtoken
 			e = new Email(email);
 			return (new Result(true, e.getClaimToken().toString()));
 		}
 		
+		//check if email has been registered
 		if(e.getClaimToken() == null)
 			return (new Result(false, "email claimed"));
 		else
@@ -104,22 +107,30 @@ public class Cli {
 	public Result register( String ct, String name, String pass, String passVer ) {
 		GraphDatabaseService graphDB = GraphDatabase.get();
 		try(Transaction tx = graphDB.beginTx()) {
-			//ResourceIterable<Node> email_nodes = graphDB.findNodesByLabelAndProperty(LabelDef.EMAIL, Email.CLAIM_TOKEN, ct);
-			
-			Email email = null;
+		
+			//check if passwords match
+			if(!pass.equals(passVer))
+				return (new Result(false, "passwords do not match"));
+				
+			Email e = null;
 			try {
-				email = new Email(Entity.findExistingNode(LabelDef.EMAIL, Email.CLAIM_TOKEN, ct));
+				//find email by claimtoken
+				e = new Email(Entity.findExistingNode(LabelDef.EMAIL, Email.CLAIM_TOKEN, ct));
 			}catch(IllegalStateException ise) {
+				//no email found, bad claimtoken
 				return (new Result(false, "bad claimtoken"));
 			}
 
+			//create or find user and set password and email
 			User u = new User();
 			u.setPassword(pass);
-			u.addEmail(email);
-			email.clearClaimToken();
+			u.addEmail(e);
+			
+			//clear claimToken(should do this on its own!!)
+			e.clearClaimToken();
 
 			tx.success();
-			return (new Result(true, "added email to user?"));
+			return (new Result(true, "registration complete"));
 			
 		}
 	}
